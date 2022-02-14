acd2a587f324fc6f0e93accdeda054a7f3cfe221
‘98hb <w17339377595@163.com>
2022-02-14 20:39:01 +0800

    请求数据,铺设表格

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import "./assets/fonts/iconfont.css"//引入字体图标css
+import "bootstrap/dist/css/bootstrap.css"
 
 Vue.config.productionTip = false
 
--- src/view/MyGoodsList.vue
+++ src/view/MyGoodsList.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>商品列表</div>
+  <div>
+    <!-- c1 父 (父传子) -->
+    <MyTable :arr="list"></MyTable>
+  </div>
 </template>
 
 <script>
-export default {};
+import MyTable from "../components/MyTable.vue";
+import axios from "axios";
+axios.defaults.baseURL = "https://www.escook.cn";
+// 目标:  循环商品列表表格
+// 1.封装MyTable.vue 整体表格组件- 一套标签和样式
+// 2.axios 请求数据
+// 3.传入MyTable 组件里循环tr显示数据
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  components: {
+    MyTable,
+  },
+  created() {
+    axios({
+      url: "/api/goods",
+    }).then((res) => {
+      // console.log(res);
+      this.list = res.data.data;
+    });
+  },
+};
 </script>
 
 <style>
